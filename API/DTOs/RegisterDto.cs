a94c328b57e1618f012cedc7654171700cee72b4
Grex Thinh <vanthinhnq@gmail.com>
2024-11-10 17:25:03 +0700

    update 6th

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,7 +4,9 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public required string Username { get; set; }
+        [Required]
         public required string Password { get; set; }
     }
 }
